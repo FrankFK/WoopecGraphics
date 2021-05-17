f530151d524b1aac1589fb1bc5e9261af724bf8e
Frank <frank.f.kruse@gmail.com>
2021-05-17 20:28:58 +0200

    chore:

--- samples/WpfSample1/obj/Debug/net5.0-windows/App.g.i.cs
+++ samples/WpfSample1/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace WpfSample1 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace WpfSample1 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
         public static void Main() {
             WpfSample1.App app = new WpfSample1.App();
             app.InitializeComponent();
